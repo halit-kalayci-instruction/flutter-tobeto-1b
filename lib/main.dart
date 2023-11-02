dd15eac0cc9c5c31ec5f54d964d1c55bd18cb3d2
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-11-02 14:48:07 +0300

    latest

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,14 @@ void main() {
   runApp(const MaterialApp(home: HomeScreen()));
 }
 
+// boilerplate => basmakalıp
 class HomeScreen extends StatelessWidget {
+  String text = "Merhaba";
   const HomeScreen({super.key}); // 1. gereksinim
+
+  void changeText() {
+    text = "Text değişti";
+  }
 
   // 2. gereksinim
   // Hot Reload => Restarta gerek kalmadan (spesifik durumlar hariç)
@@ -18,13 +24,56 @@ class HomeScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             // asset
-            Image.asset("assets/images/quiz-logo.png"),
+            Image.asset(
+              "assets/images/quiz-logo.png",
+              width: 240,
+            ),
             // Image.network(
             //     "https://miro.medium.com/v2/resize:fit:720/format:webp/1*FBRsnCP9wE84UVW1Kkv5Yw.jpeg"),
-            const Text("Quiz App"),
-            const Text("Deneme")
+            Text(text,
+                style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.5)),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.amber, foregroundColor: Colors.black),
+              child: const Text(
+                "Oyuna Başla",
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+            )
           ],
         ),
+      ),
+    );
+  }
+}
+
+// SABIT UI
+
+// StatefullWidget
+
+// 2 class
+// Widget - State
+class QuizScreen extends StatefulWidget {
+  const QuizScreen({super.key});
+
+  @override
+  State<QuizScreen> createState() {
+    return _QuizState();
+  }
+}
+
+// _State
+class _QuizState extends State<QuizScreen> {
+  @override
+  Widget build(BuildContext buildContext) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Merhaba, Quiz ekranı"),
       ),
     );
   }
